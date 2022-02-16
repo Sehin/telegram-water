b932d6671201adb8ca3091b2c9b41c15615d9d2d
Сухин Денис <sukhindk@it2g.ru>
2022-02-16 18:38:54 +0300

    add bot core

---
+++ water_bot.py
@@ -0,0 +1,39 @@
+import logging
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CallbackContext, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+def start(update: Update, context: CallbackContext):
+    """Reply on /start command"""
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        'Hi\!\n'
+        'I want to count how much water you drink\.\n'
+        'Just send number to me\!\n'
+        'If you want to check today value \- use /today command',
+        reply_markup=ForceReply(selective=True)
+    )
+
+def today(update: Update, context: CallbackContext):
+    """Send how much water user drinks today"""
+    # in progress
+    pass
+
+def main():
+    updater = Updater("TOKEN_HERE")
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
